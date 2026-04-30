e929babaa64f6329b6baf5f037ac89a9f2ed390c
mdsahilhussain <mdsahil.hussain7@gmail.com>
2026-04-30 17:15:56 +0530

    Refactor layout to use custom fonts and remove themed view component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import "@/globals.css";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  const [fontLoaded] = useFonts({
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+  })
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontLoaded])
+
+  if (!fontLoaded) return null
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
